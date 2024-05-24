e62d3c8a48e50b6ef619566a49ddc1e727248f72
bbqchickenrobot <bbqchickenrobot@lunaswap.io>
2024-05-23 19:56:21 -0500

    misc update(s)

--- TokenLending/obj/Debug/net8.0/TokenLending.AssemblyInfo.cs
+++ TokenLending/obj/Debug/net8.0/TokenLending.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright (c) 2023 Microbian Systems")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0-alpha+a2cb6136e0088dc779d928da0bc84749654fa150")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0-alpha+23f5eab1a9d6fded6d17bb4e67aa132a31a69d60")]
 [assembly: System.Reflection.AssemblyProductAttribute("TokenLending")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TokenLending")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
